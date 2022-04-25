e2fb9e5c090919bdf62ebaabb330ba2dec60bc7d
iri1980 <104352368+iri1980@users.noreply.github.com>
2022-04-25 12:05:12 +0100

    Building the board game
    i add some more rows and colomns

--- board game.py
+++ board game.py
@@ -1,9 +1,15 @@
 # Write your code here :-)
+
+def getmove():
+    row = input("my move, which row: ")
+    column = input("my move, which column")
+    
 def displayBoard():
-    print("    |  |   ")
-    print("____________")
-    print("    |  |   ")
-    print("____________")
-    print("    |  |   ")
+    print("  A  | B |   C ")
+    print("1____|___|_____")
+    print("     |   |     ")
+    print("2____|___|_____")
+    print("3    |   |     ")
 
 displayBoard()
+getmove()
