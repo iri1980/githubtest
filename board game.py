a953630d5faf53676f568458daea15fc2c4a97f4
iri1980 <104352368+iri1980@users.noreply.github.com>
2022-04-25 11:43:56 +0100

    board game
    this is a simple board game called noughts and crosses

---
+++ board game.py
@@ -0,0 +1,9 @@
+# Write your code here :-)
+def displayBoard():
+    print("    |  |   ")
+    print("____________")
+    print("    |  |   ")
+    print("____________")
+    print("    |  |   ")
+
+displayBoard()
